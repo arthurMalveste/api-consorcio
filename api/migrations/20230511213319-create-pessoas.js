b2787702f643a65448bc486a7c5c9e7536f02b62
Arthur Malveste <113934054+arthurMalveste@users.noreply.github.com>
2023-05-16 18:29:59 -0300

    Add files via upload

---
+++ api/migrations/20230511213319-create-pessoas.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pessoas', {
+      id: {
+        allowNull: false, //não pode ser um campo nulo
+        autoIncrement: true, // vai se auto encrementar 
+        primaryKey: true, // é a chave primária
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      ativo: {
+        type: Sequelize.BOOLEAN
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pessoas');
+  }
+};
